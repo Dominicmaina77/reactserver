3abe1c3618e47253337bbbd4e8ac077f1e9c87b5
Dominicmaina77 <mainadominic628@gamil.com>
2024-11-06 14:57:13 +0300

    vercelfile

--- route/userRoutes.js
+++ route/userRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const { createUser } = require('../controller/UserController');
+const { createUser, findUser, deleteUser, updateUser } = require('../controller/UserController');
 const router = express.Router();
 router.use(express.urlencoded({extended:true}));
 router.use(express.json());
 
 router.post('/createuser', createUser)
+router.get('/finduser', findUser )
+router.delete('/deleteuser', deleteUser)
+router.put('/updateuser', updateUser)
 
 module.exports = router;
